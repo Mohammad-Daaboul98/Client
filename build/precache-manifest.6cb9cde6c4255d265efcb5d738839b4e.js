d52f6de03d54a446810331436349d5ee2c33c682
Recon <mohammad.daaboul.99@gmail.com>
2021-02-17 22:26:19 +0200

    new client side

---
+++ build/precache-manifest.6cb9cde6c4255d265efcb5d738839b4e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1bd388d17d3a9cf2b5b84dbd377efa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb0cbcd1118509c6e8cf",
+    "url": "./static/js/2.76e90ab5.chunk.js"
+  },
+  {
+    "revision": "ed3d9bf827d278ef04e6",
+    "url": "./static/js/main.3dd0fa64.chunk.js"
+  },
+  {
+    "revision": "7dd1b07ef8cef8e5718b",
+    "url": "./static/js/runtime-main.79c9e6b2.js"
+  }
+]);
---
+++ src/components/App.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import Axios from "axios";
+
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  // const [id ,setId] = useState();
+
+
+
+
+
+  useEffect(() => {
+    Axios.get("https://my-note-app-98.herokuapp.com//api/get").then((result) => {
+      setNotes(result.data)
+    })
+  }, [AddNote]);
+
+  function AddNote(newNote) {
+
+    Axios.post("https://my-note-app-98.herokuapp.com//api/insert", {
+      title: newNote.title,
+      content: newNote.content
+    }).then("Susccessful insert");
+  
+
+
+    // setNotes(prevNotes => {
+    //   return [...prevNotes, newNote];
+    // });
+  }
+
+  function deleteNote(id) {
+
+    Axios.delete(`https://my-note-app-98.herokuapp.com//api/delete/${id}`);
+    // setNotes(prevNotes => {
+    //   return prevNotes.filter((noteItem, index) => {
+    //     return index !== Id;
+    //   });
+    // });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={AddNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={noteItem._id}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
